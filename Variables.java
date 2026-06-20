b7d349c3193c3a82c881cd22aaab3599785dcb25
jahid45780 <jahidhossain88899@gmail.com>
2026-06-21 01:11:00 +0600

    Variables

---
+++ Variables.java
@@ -0,0 +1,13 @@
+
+
+public class Variables{
+    public static void main(String[] args) {
+        String name ="Jahid";
+        int age= 25;
+        String country = "Bangladesh";
+
+        System.out.println("Name:" +name);
+        System.out.println("Age:" +age);
+        System.out.println("country:" +country);
+    }
+}
--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 
-public class main {
+public class Main {
     public static void main(String[] args){
-        System.out.println("hello java developer ");
+        System.out.println("Name: Jahid");
+        System.out.println("Profession: devloper");
+        System.out.println("Country: Bangladesh");
     }
 }
